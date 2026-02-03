46c848d14b676b36453fb3dbc8aac7e025c07e4f
Pppham <kmitzel602@gmail.com>
2026-02-03 13:32:49 -0800

    added datasets

--- data.R
+++ data.R
@@ -1,3 +1,17 @@
-# Test file
+# Data: Loaded the datasets and libraries
 
+library(here)
+library(dplyr)
 library(ggplot2)
+
+
+
+
+#Datasets
+
+pred_temp_df <- read.csv(here("data", "Temp_GCMs.csv"))
+NC_gas_df <- read.csv(here("data", "NG_CONS_SUM_DCU_SNC_M.csv"))
+
+RDU_df <- read.csv(here("data", "USW00013722.csv"))
+GSO_df <- read.csv(here("data", "USW00013881.csv"))
+CLT_df <- read.csv(here("data", "USW00013881.csv"))
