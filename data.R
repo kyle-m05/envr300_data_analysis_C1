00576b9ac4208d6b1f2d4403647b106d18a85fa4
Pppham <kmitzel602@gmail.com>
2026-02-05 11:51:18 -0800

    renamed column names

--- data.R
+++ data.R
@@ -13,7 +13,10 @@ NC_gas_df <- read.csv(here("data", "NG_CONS_SUM_DCU_SNC_M.csv"))
 NC_gas_df <- NC_gas_df[-1, ] %>%
   select(Back.to.Contents,
          X,
-         X.1)
+         X.1) %>%
+  rename(date = Back.to.Contents ,
+         residential = X,
+         commercial = X.1)
 #airport weather dataframes
 RDU_df <- read.csv(here("data", "USW00013722.csv"))
 GSO_df <- read.csv(here("data", "USW00013881.csv"))
