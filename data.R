dd4ff81503420285d181257344e97ef1cde9d787
Pppham <kmitzel602@gmail.com>
2026-02-05 12:19:14 -0800

    cleaned airport data

--- data.R
+++ data.R
@@ -19,5 +19,16 @@ NC_gas_df <- NC_gas_df[-1, ] %>%
          commercial = X.1)
 #airport weather dataframes
 RDU_df <- read.csv(here("data", "USW00013722.csv"))
+
+RDU_df <- RDU_df %>%
+  select(DATE, CLDD, HTDD, TAVG, TMAX, TMIN)
+
 GSO_df <- read.csv(here("data", "USW00013881.csv"))
+
+GSO_df <- GSO_df %>%
+  select(DATE, CLDD, HTDD, TAVG, TMAX, TMIN)
+
 CLT_df <- read.csv(here("data", "USW00013881.csv"))
+
+CLT_df <- CLT_df %>%
+  select(DATE, CLDD, HTDD, TAVG, TMAX, TMIN)
