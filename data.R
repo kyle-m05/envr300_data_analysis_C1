fea54b0e0ca34d6e42f07e21d878d30caf6fde4e
Pppham <kmitzel602@gmail.com>
2026-02-05 15:21:34 -0800

    averaging airport data changes

--- data.R
+++ data.R
@@ -17,18 +17,35 @@ NC_gas_df <- NC_gas_df[-1, ] %>%
   rename(date = Back.to.Contents ,
          residential = X,
          commercial = X.1)
+
 #airport weather dataframes
 RDU_df <- read.csv(here("data", "USW00013722.csv"))
 
 RDU_df <- RDU_df %>%
-  select(DATE, CLDD, HTDD, TAVG, TMAX, TMIN)
+  select(DATE, CLDD, HTDD, TAVG, TMAX, TMIN) %>%
+  mutate(
+    DATE = {
+      mon <- sub("-.*", "", DATE)
+      yr2 <- as.integer(sub(".*-", "", DATE))
+      yr4 <- if_else(yr2 <= 23, 2000 + yr2, 1900 + yr2)
+      format(my(paste(mon, yr4)), "%Y-%m")
+    }
+  )
 
 GSO_df <- read.csv(here("data", "USW00013881.csv"))
 
 GSO_df <- GSO_df %>%
-  select(DATE, CLDD, HTDD, TAVG, TMAX, TMIN)
+  select(DATE, CLDD, HTDD, TAVG, TMAX, TMIN) %>%
+  mutate(DATE = format(ym(DATE), "%Y-%m"))
 
 CLT_df <- read.csv(here("data", "USW00013881.csv"))
 
 CLT_df <- CLT_df %>%
-  select(DATE, CLDD, HTDD, TAVG, TMAX, TMIN)
+  select(DATE, CLDD, HTDD, TAVG, TMAX, TMIN) %>%
+  mutate(DATE = format(ym(DATE), "%Y-%m"))
+
+
+
+
+
+
--- step_2.R
+++ step_2.R
@@ -1,2 +1,29 @@
 #averaging the weather data from the three airports
 
+RDU_df_rename <- RDU_df %>%
+    rename(CLDD_RDU = CLDD,
+           HTDD_RDU = HTDD,
+           TAVG_RDU = TAVG,
+           TMAX_RDU = TMAX,
+           TMIN_RDU = TMIN)
+
+CLT_df_rename <- CLT_df %>%
+  rename(CLDD_CLT = CLDD,
+         HTDD_CLT = HTDD,
+         TAVG_CLT = TAVG,
+         TMAX_CLT = TMAX,
+         TMIN_CLT = TMIN)
+
+GSO_df_rename <- GSO_df %>%
+  rename(CLDD_GSO = CLDD,
+         HTDD_GSO = HTDD,
+         TAVG_GSO = TAVG,
+         TMAX_GSO = TMAX,
+         TMIN_GSO = TMIN)
+
+combined_df <- full_join(CLT_df_rename, GSO_df_rename, by = "DATE")
+
+combined_df <- full_join(airport_df, RDU_df_rename, by = "DATE")
+
+
+
