6afca74b4c4b4ad8a6181683c427c3e53bf48a57
Pppham <kmitzel602@gmail.com>
2026-02-06 12:49:00 -0800

    plots for warm cold mths completed

--- step_1.R
+++ step_1.R
@@ -4,6 +4,8 @@
 residential_plot <- NC_gas_df %>%
   ggplot() +
   geom_point(aes(x = date, y = residential)) +
+  geom_smooth(aes(x = date, y = residential), 
+              method = "lm", formula = y ~ x, se = FALSE) +
   labs(
     x = "Date",
     y = "Natural Gas Consumption (MMcf)",
@@ -17,7 +19,7 @@ residential_plot
 
 residential_lm <- lm(residential ~ date, data = NC_gas_df)
 
-summary(residential_lm)
+tidy(residential_lm)
 
 
 #overall commercial consumption plot
@@ -25,6 +27,7 @@ summary(residential_lm)
 commercial_plot <- NC_gas_df %>%
   ggplot() +
   geom_point(aes(x = date, y = commercial)) +
+  geom_smooth(aes(x = date, y = commercial), method = "lm", formula = y ~ x, se = FALSE) +
   labs(
     x = "Date",
     y = "Natural Gas Consumption (MMcf)",
@@ -38,14 +41,71 @@ commercial_plot
 
 commercial_lm <- lm(commercial ~ date, data = NC_gas_df)
 
-summary(commercial_lm)
+tidy(commercial_lm)
 
+#splitting the months into 'warm' and 'cold' months
 
+NC_gas_cold_df <- NC_gas_df %>%
+  filter(month(date) %in% c(1,2,3,4,11,12))
 
+NC_gas_warm_df <- NC_gas_df %>%
+  filter(month(date) %in% c(5:10))
 
+#creating plots for warm months
 
+residential_warm_plot <- NC_gas_warm_df %>%
+  ggplot() +
+  geom_point(aes(x = date, y = residential)) +
+  geom_smooth(aes(x = date, y = residential), 
+              method = "lm", formula = y ~ x, se = FALSE) +
+  labs(
+    x = "Date",
+    y = "Natural Gas Consumption (MMcf)",
+    title = "Residential Natural Gas Consumption of 'Warm' Months"
+  )
 
+residential_warm_plot
 
+commercial_warm_plot <- NC_gas_warm_df %>%
+  ggplot() +
+  geom_point(aes(x = date, y = commercial)) +
+  geom_smooth(aes(x = date, y = commercial), 
+              method = "lm", formula = y ~ x, se = FALSE) +
+  labs(
+    x = "Date",
+    y = "Natural Gas Consumption (MMcf)",
+    title = "Commercial Natural Gas Consumption of 'Warm' Months"
+  )
+
+commercial_warm_plot
+
+#creating plots for cold months
+
+residential_cold_plot <- NC_gas_cold_df %>%
+  ggplot() +
+  geom_point(aes(x = date, y = residential)) +
+  geom_smooth(aes(x = date, y = residential), 
+              method = "lm", formula = y ~ x, se = FALSE) +
+  labs(
+    x = "Date",
+    y = "Natural Gas Consumption (MMcf)",
+    title = "Residential Natural Gas Consumption of 'Cold' Months"
+  )
+
+residential_cold_plot
+
+commercial_cold_plot <- NC_gas_cold_df %>%
+  ggplot() +
+  geom_point(aes(x = date, y = commercial)) +
+  geom_smooth(aes(x = date, y = commercial), 
+              method = "lm", formula = y ~ x, se = FALSE) +
+  labs(
+    x = "Date",
+    y = "Natural Gas Consumption (MMcf)",
+    title = "Commercial Natural Gas Consumption of 'Cold' Months"
+  )
+
+commercial_cold_plot
 
 
 
